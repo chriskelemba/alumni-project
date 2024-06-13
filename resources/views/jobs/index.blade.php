fe7f93d688399a49779d18f055e5e172cf916ff6
chriskelemba <loverhxchris@gmail.com>
2024-06-13 14:01:39 +0300

    job style edit

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -14,21 +14,13 @@
                     </div>
                     <div class="overflow-x-auto">
                         <table class="w-full text-sm text-center text-gray-500">
-                            <thead class="text-xs text-gray-700 uppercase bg-gray-50">
-                                <tr>
-                                    <th class="py-3 px-6">Id</th>
-                                    <th class="py-3 px-6">Title</th>
-                                    <th class="py-3 px-6">Description</th>
-                                    <th class="py-3 px-6">Action</th>
-                                </tr>
-                            </thead>
                             <tbody>
                                 @foreach ($jobs as $job)
-                                <tr class="bg-white border-b">
-                                    <td class="py-4 px-6">{{ $job->id }}</td>
+                                <tr class="bg-gray-100 border-b">
+                                    <td class="py-4 px-6 text-xs text-gray-700 uppercase">{{ $job->id }}</td>
                                     <td class="py-4 px-6">{{ $job->title }}</td>
                                     <td class="py-4 px-6">{{ $job->description }}</td>
-                                    <td class="py-4 px-6">
+                                    <td class="py-4 px-6 flex">
                                             <a href="{{ url('jobs/'.$job->id.'/edit') }}" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">Edit</a>
                                             <a href="{{ url('jobs/'.$job->id.'/delete') }}" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded" onclick="return confirm('Are you sure you want to delete this job?')">Delete</a>
                                     </td>
@@ -37,9 +29,9 @@
                             </tbody>
                         </table>
                     </div>
-                        <div class="text-center mb-4 mt-6">
-                            <a href="{{ url('jobs/trash') }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Recycling Bin</a>
-                        </div>
+                    <div class="text-center mb-4 mt-6">
+                        <a href="{{ url('jobs/trash') }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Recycling Bin</a>
+                    </div>
                 </div>
             </div>
         </div>
